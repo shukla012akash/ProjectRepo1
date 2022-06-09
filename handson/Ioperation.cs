927c45158ab098d2644daaf88e0f7ec182d6e72b
shukla01akash <as1999shukla01@gmail.com>
2022-06-09 16:47:07 -0700

    handson Practice

---
+++ handson/Ioperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handson
+{
+    public interface Ioperation
+    {
+        int add(int a, int b);
+        int multiply(int  a,int b);
+        int sub(int a, int b);
+        int div(int a,int b);
+    }
+}
---
+++ handson/Operation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handson
+{
+    public class Operation
+    {
+        public int Addition(int a, int b)
+        {
+        return a + b;
+            }
+        public int Multiply(int a, int b)
+        {
+            return a*b;
+        }
+        public int Substraction(int a, int b)
+        {
+            return a - b;
+        }
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
