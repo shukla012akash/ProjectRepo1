4b446975b3ada1081ed847eaa28b3b5e4ff8811f
shukla01akash <as1999shukla01@gmail.com>
2022-06-10 17:34:54 -0700

    Reflection handson

---
+++ Reflectionhandson/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+
+namespace Reflectionhandson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student obj = new Student();
+            Type type = typeof(Student);
+            var member = type.GetMembers();
+            Console.WriteLine($"Following {member.Count()} members are available for Student :");
+            foreach (var member2 in member)
+            {
+                Console.WriteLine($"(member");
+            }
+            Console.WriteLine();
+            var prop = type.GetProperties();
+            Console.WriteLine($"Following {prop.Count()} properties are available for Student :");
+            foreach (var prop2 in prop)
+            {
+                Console.WriteLine(prop);
+            }
+            Console.WriteLine();
+            var method = type.GetMethods();
+            Console.WriteLine($"Following {method.Count()} method are available for Student :");
+            foreach (var method2 in method)
+            {
+                Console.WriteLine(method);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+            
+          
---
+++ Reflectionhandson/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflectionhandson
+{
+    internal class Student
+    {
+       public int RollNo
+        {
+            get; set;
+        }
+        public string Name
+        { get; set; }
+
+        // no argument
+        public Student()
+        {
+            RollNo = 0;
+            Name = string.Empty;
+        }
+
+        // parameter constructor
+        public Student(int rno, string n)
+        {
+            RollNo = rno;
+            Name = n;
+
+        }
+        public void displayData()
+        {
+            Console.WriteLine("Roll Number : {0}",RollNo);
+            Console.WriteLine("Name : {0}",Name);
+        }
+    }
+}
+
